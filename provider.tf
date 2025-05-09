5851580f93f63c1a517f3806c8a61a4584814306
Tejpal88 <90080347+Tejpal88@users.noreply.github.com>
2025-05-09 21:54:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,9 +11,6 @@ provider "azurerm" {
   features {
 
   }
-  client_id       = "ff2de01d-97f6-45c8-bf10-018dcfd5a905"
-  client_secret   = "oRy8Q~PlB2PEJyfJLaRD18J1jkpGrxUDz~bXmdsI"
-  tenant_id       = "90d12e33-ad5e-4e82-8b5e-82bff0658ae0"
-  subscription_id = "a27dbdef-1fae-46c8-b2ce-ad5d84e1b13e"
+
 
 }
