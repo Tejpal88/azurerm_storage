ee5fcfd82fe122d8c8c5a5b965d527d7739a06cd
Tejpal Singh <tejpalsingh.singh@gmail.com>
2026-03-01 21:43:28 +0530

    location changed to eastus 2

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "rgs" {
   name     = "test-rg"
-  location = "westus"
+  location = "eastus 2"
 }
 resource "azurerm_storage_account" "stgs" {
   name                     = "azstorage788756"
   resource_group_name      = "test-rg"
-  location                 = "westus"
+  location                 = "eastus 2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
