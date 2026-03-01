be6d8c5c71a5d55f473aea421c7d300d736ab26a
Tejpal Singh <90080347+Tejpal88@users.noreply.github.com>
2026-03-01 21:52:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 resource "azurerm_resource_group" "rgs" {
   name     = "test-rg"
-  location = "eastus 2"
+  # location = "eastus 2"
   location = "Central India"
 }
 resource "azurerm_storage_account" "stgs" {
   name                     = "azstorage788756"
   resource_group_name      = "test-rg"
-  location                 = "eastus 2"
+  # location                 = "eastus 2"
   location                 = "Central India"
   account_tier             = "Standard"
   account_replication_type = "GRS"
+
 
 }
